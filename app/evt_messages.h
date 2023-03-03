94618fbb229caa0083e9c03ec904ed883524cfcc
Jonas Remmert <j.remmert@phytec.de>
2023-03-04 00:56:26 +0100

    app example: Create a simple app to demonstrate ZBUS
    The ZBUS app consists of two modules, main and the led_module. The main
    module sends a message to the led_module via ZBUS on button press. On
    message receive, the led_module toggles an led.
    
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

---
+++ app/evt_messages.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Phytec Messtechnik GmbH
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef EVT_MESSAGES_H_
+#define EVT_MESSAGES_H_
+
+#include <stdint.h>
+
+struct bp_msg {
+	uint32_t pin;
+};
+
+#endif /* EVT_MESSAGES_H_ */
